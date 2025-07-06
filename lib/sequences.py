b9e6eb48b5649604d08023b425f72e9a8052ea60
mathiasmunene <mathiasmunene@gmail.com>
2025-07-06 23:59:22 +0300

    commit 2

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-fib_list = []
+    fib_list = []
     if length <= 0:
         print(fib_list)
+        return
+    elif length == 1:
+        fib_list = [0]
+        print(fib_list)
+        return
